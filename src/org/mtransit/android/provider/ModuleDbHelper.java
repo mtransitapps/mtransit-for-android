17f9de4b97f9fd4e7a16d770f5fd77d2ca17fb9c
Mathieu Méa <mathieu.mea@gmail.com>
2015-01-23 15:13:39 -0500

    Bump DB version for API changes.

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 17;
+	public static final int DB_VERSION = 18;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
