64be28e8bbc6e32a7cdb4024e674a66edb27f9d7
Mathieu Méa <mathieu.mea@gmail.com>
2015-02-22 19:40:14 -0500

    Missing part (6).

---
+++ src/org/mtransit/android/task/MapPOILoader.java
@@ -0,0 +1,206 @@
+package org.mtransit.android.task;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.mtransit.android.commons.CollectionUtils;
+import org.mtransit.android.commons.MTLog;
+import org.mtransit.android.commons.RuntimeUtils;
+import org.mtransit.android.commons.data.RouteTripStop;
+import org.mtransit.android.commons.provider.POIFilter;
+import org.mtransit.android.commons.task.MTCallable;
+import org.mtransit.android.data.AgencyProperties;
+import org.mtransit.android.data.DataSourceManager;
+import org.mtransit.android.data.DataSourceProvider;
+import org.mtransit.android.data.DataSourceType;
+import org.mtransit.android.data.POIManager;
+import org.mtransit.android.ui.view.MapViewController;
+
+import android.content.Context;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+public class MapPOILoader extends MTAsyncTaskLoaderV4<Collection<MapViewController.POIMarker>> {
+
+	private static final String TAG = MapPOILoader.class.getSimpleName();
+
+	@Override
+	public String getLogTag() {
+		return TAG;
+	}
+
+	private Collection<Integer> filterTypeIds;
+	private LatLngBounds latLngBounds;
+	private LatLngBounds loadedLatLngBounds;
+
+	private Collection<MapViewController.POIMarker> poiMarkers;
+
+	public MapPOILoader(Context context, Collection<Integer> filterTypeIds, LatLngBounds latLngBounds, LatLngBounds loadedLatLngBounds) {
+		super(context);
+		this.filterTypeIds = filterTypeIds;
+		this.latLngBounds = latLngBounds;
+		this.loadedLatLngBounds = loadedLatLngBounds;
+	}
+
+	@Override
+	public Collection<MapViewController.POIMarker> loadInBackgroundMT() {
+		if (this.poiMarkers != null) {
+			return this.poiMarkers;
+		}
+		this.poiMarkers = new HashSet<MapViewController.POIMarker>();
+		ArrayList<AgencyProperties> agencies = DataSourceProvider.get(getContext()).getAllAgencies(getContext());
+		if (CollectionUtils.getSize(agencies) == 0) {
+			return this.poiMarkers;
+		}
+		if (this.latLngBounds == null) {
+			return this.poiMarkers;
+		}
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(RuntimeUtils.NUMBER_OF_CORES, RuntimeUtils.NUMBER_OF_CORES, 1, TimeUnit.SECONDS,
+				new LinkedBlockingDeque<Runnable>(agencies.size()));
+		ArrayList<Future<HashMap<LatLng, MapViewController.POIMarker>>> taskList = new ArrayList<Future<HashMap<LatLng, MapViewController.POIMarker>>>();
+		for (AgencyProperties agency : agencies) {
+			DataSourceType type = agency.getType();
+			if (!type.isMapScreen()) {
+				continue;
+			}
+			if (!agency.isInArea(this.latLngBounds)) {
+				continue;
+			}
+			if (CollectionUtils.getSize(this.filterTypeIds) > 0 && !this.filterTypeIds.contains(type.getId())) {
+				continue;
+			}
+			FindAgencyPOIsTask task = new FindAgencyPOIsTask(getContext(), agency, this.latLngBounds, this.loadedLatLngBounds);
+			taskList.add(executor.submit(task));
+		}
+		HashMap<LatLng, MapViewController.POIMarker> positionToPoiMarkers = new HashMap<LatLng, MapViewController.POIMarker>();
+		for (Future<HashMap<LatLng, MapViewController.POIMarker>> future : taskList) {
+			try {
+				HashMap<LatLng, MapViewController.POIMarker> agencyPOIs = future.get();
+				if (agencyPOIs != null) {
+					for (HashMap.Entry<LatLng, MapViewController.POIMarker> agencyMarker : agencyPOIs.entrySet()) {
+						if (positionToPoiMarkers.containsKey(agencyMarker.getKey())) {
+							positionToPoiMarkers.get(agencyMarker.getKey()).merge(agencyMarker.getValue());
+						} else {
+							positionToPoiMarkers.put(agencyMarker.getKey(), agencyMarker.getValue());
+						}
+					}
+				}
+			} catch (Exception e) {
+				MTLog.w(this, e, "Error while loading in background!");
+			}
+		}
+		executor.shutdown();
+		this.poiMarkers = positionToPoiMarkers.values();
+		return this.poiMarkers;
+	}
+
+	@Override
+	protected void onStartLoading() {
+		super.onStartLoading();
+		if (this.poiMarkers != null) {
+			deliverResult(this.poiMarkers);
+		}
+		if (this.poiMarkers == null) {
+			forceLoad();
+		}
+	}
+
+	@Override
+	protected void onStopLoading() {
+		super.onStopLoading();
+		cancelLoad();
+	}
+
+	@Override
+	public void deliverResult(Collection<MapViewController.POIMarker> data) {
+		this.poiMarkers = data;
+		if (isStarted()) {
+			super.deliverResult(data);
+		}
+	}
+
+	private static class FindAgencyPOIsTask extends MTCallable<HashMap<LatLng, MapViewController.POIMarker>> {
+
+		private static final String TAG = MapPOILoader.class.getSimpleName() + ">" + FindAgencyPOIsTask.class.getSimpleName();
+
+		@Override
+		public String getLogTag() {
+			return TAG;
+		}
+
+		private Context context;
+		private AgencyProperties agency;
+		private LatLngBounds latLngBounds;
+		private LatLngBounds loadedLatLngBounds;
+
+		public FindAgencyPOIsTask(Context context, AgencyProperties agency, LatLngBounds latLngBounds, LatLngBounds loadedLatLngBounds) {
+			this.context = context;
+			this.agency = agency;
+			this.latLngBounds = latLngBounds;
+			this.loadedLatLngBounds = loadedLatLngBounds;
+		}
+
+		@Override
+		public HashMap<LatLng, MapViewController.POIMarker> callMT() throws Exception {
+			double minLat = Math.min(this.latLngBounds.northeast.latitude, this.latLngBounds.southwest.latitude);
+			double maxLat = Math.max(this.latLngBounds.northeast.latitude, this.latLngBounds.southwest.latitude);
+			double minLng = Math.min(this.latLngBounds.northeast.longitude, this.latLngBounds.southwest.longitude);
+			double maxLng = Math.max(this.latLngBounds.northeast.longitude, this.latLngBounds.southwest.longitude);
+			Double optLoadedMinLat = this.loadedLatLngBounds == null ? null : Math.min(this.loadedLatLngBounds.northeast.latitude,
+					this.loadedLatLngBounds.southwest.latitude);
+			Double optLoadedMaxLat = this.loadedLatLngBounds == null ? null : Math.max(this.loadedLatLngBounds.northeast.latitude,
+					this.loadedLatLngBounds.southwest.latitude);
+			Double optLoadedMinLng = this.loadedLatLngBounds == null ? null : Math.min(this.loadedLatLngBounds.northeast.longitude,
+					this.loadedLatLngBounds.southwest.longitude);
+			Double optLoadedMaxLng = this.loadedLatLngBounds == null ? null : Math.max(this.loadedLatLngBounds.northeast.longitude,
+					this.loadedLatLngBounds.southwest.longitude);
+			POIFilter poiFilter = new POIFilter(minLat, maxLat, minLng, maxLng, optLoadedMinLat, optLoadedMaxLat, optLoadedMinLng, optLoadedMaxLng);
+			HashMap<LatLng, MapViewController.POIMarker> clusterItems = new HashMap<LatLng, MapViewController.POIMarker>();
+			ArrayList<POIManager> poims = DataSourceManager.findPOIs(this.context, this.agency.getAuthority(), poiFilter);
+			String agencyShortName = this.agency.getShortName();
+			if (poims != null) {
+				LatLng position;
+				String name;
+				String extra;
+				String uuid;
+				String authority;
+				Integer color;
+				Integer secondaryColor;
+				int nbUseless = 0;
+				for (POIManager poim : poims) {
+					position = new LatLng(poim.poi.getLat(), poim.poi.getLng());
+					if (!this.latLngBounds.contains(position)) {
+						nbUseless++;
+						continue;
+					} else if (this.loadedLatLngBounds != null && this.loadedLatLngBounds.contains(position)) {
+						nbUseless++;
+						continue;
+					}
+					name = poim.poi.getName();
+					extra = null;
+					if (poim.poi instanceof RouteTripStop) {
+						extra = ((RouteTripStop) poim.poi).getRoute().getShortestName();
+					}
+					uuid = poim.poi.getUUID();
+					authority = poim.poi.getAuthority();
+					color = POIManager.getColor(this.context, poim.poi, null);
+					secondaryColor = agency.getColorInt();
+					if (clusterItems.containsKey(position)) {
+						clusterItems.get(position).merge(name, agencyShortName, extra, color, secondaryColor, uuid, authority);
+					} else {
+						clusterItems.put(position, new MapViewController.POIMarker(position, name, agencyShortName, extra, color, secondaryColor, uuid,
+								authority));
+					}
+				}
+			}
+			return clusterItems;
+		}
+	}
+}
---
+++ src/org/mtransit/android/task/POIsLoader.java
@@ -0,0 +1,94 @@
+package org.mtransit.android.task;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.mtransit.android.commons.provider.POIFilter;
+import org.mtransit.android.data.DataSourceManager;
+import org.mtransit.android.data.POIManager;
+
+import android.content.Context;
+
+public class POIsLoader extends MTAsyncTaskLoaderV4<ArrayList<POIManager>> {
+
+	private static final String TAG = POIsLoader.class.getSimpleName();
+
+	@Override
+	public String getLogTag() {
+		return TAG;
+	}
+
+	private ArrayList<String> uuids;
+	private ArrayList<String> authorities;
+	private ArrayList<POIManager> pois;
+
+	public POIsLoader(Context context, ArrayList<String> uuids, ArrayList<String> authorities) {
+		super(context);
+		this.uuids = uuids;
+		this.authorities = authorities;
+	}
+
+	@Override
+	public ArrayList<POIManager> loadInBackgroundMT() {
+		if (this.pois != null) {
+			return this.pois;
+		}
+		this.pois = new ArrayList<POIManager>();
+		HashMap<String, HashSet<String>> authorityToUUIDs = splitByAgency(this.uuids, this.authorities);
+		if (authorityToUUIDs != null && authorityToUUIDs.size() > 0) {
+			for (String authority : authorityToUUIDs.keySet()) {
+				HashSet<String> authorityUUIDs = authorityToUUIDs.get(authority);
+				if (authorityUUIDs != null && authorityUUIDs.size() > 0) {
+					ArrayList<POIManager> agencyPOIs = DataSourceManager.findPOIs(getContext(), authority, new POIFilter(authorityUUIDs));
+					if (agencyPOIs != null) {
+						Collections.sort(agencyPOIs, POIManager.POI_ALPHA_COMPARATOR);
+						this.pois.addAll(agencyPOIs);
+					}
+				}
+			}
+		}
+		return this.pois;
+	}
+
+	private HashMap<String, HashSet<String>> splitByAgency(ArrayList<String> uuids, ArrayList<String> authorities) {
+		HashMap<String, HashSet<String>> authorityToUUIDs = new HashMap<String, HashSet<String>>();
+		if (uuids != null) {
+			for (int i = 0; i < uuids.size(); i++) {
+				String uuid = uuids.get(i);
+				String authority = authorities.get(i);
+				if (!authorityToUUIDs.containsKey(authority)) {
+					authorityToUUIDs.put(authority, new HashSet<String>());
+				}
+				authorityToUUIDs.get(authority).add(uuid);
+			}
+		}
+		return authorityToUUIDs;
+	}
+
+	@Override
+	protected void onStartLoading() {
+		super.onStartLoading();
+		if (this.pois != null) {
+			deliverResult(this.pois);
+		}
+		if (this.pois == null) {
+			forceLoad();
+		}
+	}
+
+	@Override
+	protected void onStopLoading() {
+		super.onStopLoading();
+		cancelLoad();
+	}
+
+	@Override
+	public void deliverResult(ArrayList<POIManager> data) {
+		this.pois = data;
+		if (isStarted()) {
+			super.deliverResult(data);
+		}
+	}
+}
