404b11a2a90ee49edc21e8da3aef727be45c331b
Mathieu Méa <mathieu.mea@gmail.com>
2015-03-12 19:31:12 -0400

    Minor improvement to modules names & bump DB version

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 20;
+	public static final int DB_VERSION = 21;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
