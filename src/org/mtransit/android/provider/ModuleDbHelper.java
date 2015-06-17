b41214dc7e6db0f9a1a4ed6153ae8cadb57458cb
Mathieu Méa <mathieu.mea@gmail.com>
2015-06-17 15:22:43 -0400

    Add St Catharines Transit to agency list & bump DB version

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 26;
+	public static final int DB_VERSION = 27;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
