4fa097782d659b4de5e4cd7a7ada777169faffa2
Mathieu Méa <mathieu.mea@gmail.com>
2015-06-25 17:25:38 -0400

    Add Welland Transit to agency list & bump DB version

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 27;
+	public static final int DB_VERSION = 28;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
