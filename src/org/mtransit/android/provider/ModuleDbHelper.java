c6b19ab79c33e96cf5f46072e265b1a1d12f6e94
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-03 11:45:53 -0400

    Add Brandon Transit to agency list & bump DB version

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 29;
+	public static final int DB_VERSION = 30;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
