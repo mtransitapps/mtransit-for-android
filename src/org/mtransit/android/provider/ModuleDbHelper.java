b487721fb535afa948490b67aa62160d50ad95bf
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-27 09:49:55 -0400

    Add Edmonton ETS Bus & LRT, St Albert Transit & Strathcona County Transit to agency list & bump DB version

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 32;
+	public static final int DB_VERSION = 33;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
