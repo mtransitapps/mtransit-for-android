4905fafa09ad76f57d54363ff995a1e21fb71805
Mathieu Méa <mathieu.mea@gmail.com>
2015-08-10 10:55:33 -0400

    Add Milton Transit to agency list & bump DB version

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 34;
+	public static final int DB_VERSION = 35;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
