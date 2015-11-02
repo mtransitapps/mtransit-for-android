a9af71cc87f07421a8c94973342191d6d411cbc9
Mathieu Méa <mathieu.mea@gmail.com>
2015-11-02 13:34:15 -0500

    Add London Transit (LTC) Bus to agency list & bump DB version

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 43;
+	public static final int DB_VERSION = 44;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
