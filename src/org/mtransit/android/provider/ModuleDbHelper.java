fe5eafbcc00bbdb89f6a1b5429a8953b89e701b7
Mathieu Méa <mathieu.mea@gmail.com>
2017-06-02 06:09:02 -0400

    Add Lévis STL Bus to agency list & bump DB version

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 53;
+	public static final int DB_VERSION = 54;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
