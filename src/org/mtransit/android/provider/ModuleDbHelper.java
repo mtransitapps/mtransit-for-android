3400a710b24b5afea8c3d18ca4fbbf1b7ee9ccdd
Mathieu Méa <mathieu.mea@gmail.com>
2017-10-02 07:54:42 -0400

    Add Vernon Regional Transit System Bus to agency list & bump DB version

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 56;
+	public static final int DB_VERSION = 57;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
