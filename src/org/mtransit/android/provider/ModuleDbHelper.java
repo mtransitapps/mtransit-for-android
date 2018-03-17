8c6abe818894386fbeed30f1e92e13f8a41536c5
Mathieu Méa <mathieu.mea@gmail.com>
2018-03-17 18:40:36 -0400

    Update modules to reflect Montreal AMT => RTM change

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -26,7 +26,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 57;
+	public static final int DB_VERSION = 58;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
