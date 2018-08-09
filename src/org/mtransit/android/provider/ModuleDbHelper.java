99e8ecd9685e20382e6f8d8f64784d774393e2ab
Mathieu Méa <mathieu.mea@gmail.com>
2018-08-09 08:34:06 -0400

    Add WEGO Niagara Falls Visitor Transportation Bus to agency list & bump DB version

--- src/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/org/mtransit/android/provider/ModuleDbHelper.java
@@ -27,7 +27,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 62;
+	public static final int DB_VERSION = 63;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
