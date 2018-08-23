49660885f40fd19acfbe1a2936b3f510068a57ba
Mathieu Méa <mathieu.mea@gmail.com>
2018-08-23 15:07:42 -0400

    Improve gradlew console output for CI

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 echo ">> Building...";
+IS_CI=false;
+if [[ ! -z "${CI}" ]]; then
+	IS_CI=true;
+fi
+echo "app-android/build.sh: IS_CI '${IS_CI}'";
+GRADLE_ARGS="";
+if [ $IS_CI = true ]; then
+	GRADLE_ARGS=" --console=plain";
+fi
 DIRECTORY=$(basename ${PWD});
 CUSTOM_SETTINGS_GRADLE_FILE="../settings.gradle.all";
 if [ -f $CUSTOM_SETTINGS_GRADLE_FILE ]; then
-	../gradlew -c $CUSTOM_SETTINGS_GRADLE_FILE :$DIRECTORY:clean :$DIRECTORY:assembleRelease :$DIRECTORY:copyReleaseApkToOutputDirs;
+	../gradlew -c $CUSTOM_SETTINGS_GRADLE_FILE :$DIRECTORY:clean :$DIRECTORY:assembleRelease :$DIRECTORY:copyReleaseApkToOutputDirs $GRADLE_ARGS;
 	RESULT=$?;
 else
-	../gradlew :$DIRECTORY:clean :$DIRECTORY:assembleRelease :$DIRECTORY:copyReleaseApkToOutputDirs;
+	../gradlew :$DIRECTORY:clean :$DIRECTORY:assembleRelease :$DIRECTORY:copyReleaseApkToOutputDirs $GRADLE_ARGS;
 	RESULT=$?;
 fi
 echo ">> Building... DONE";
