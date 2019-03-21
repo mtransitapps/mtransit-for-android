bcf352a3dbfba1263e6beee93c2f7f052e9de33e
Mathieu Méa <mathieu.mea@gmail.com>
2019-03-20 20:45:50 -0400

    Only run Debug flavor unit tests

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ echo ">> Gradle cleaning... DONE";
 
 if [[ ${IS_CI} = true ]]; then
     echo ">> Running test...";
-	../gradlew ${SETTINGS_FILE_ARGS} :commons-android:test test ${GRADLE_ARGS};
+	../gradlew ${SETTINGS_FILE_ARGS} :commons-android:testDebug testDebug ${GRADLE_ARGS};
 	RESULT=$?;
 	checkResult ${RESULT};
     echo ">> Running test... DONE";
