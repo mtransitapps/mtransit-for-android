144be92f2f0f647c0b71c6232c9d33facac610e0
Mathieu Méa <mathieu.mea@gmail.com>
2019-08-06 11:25:43 -0400

    App Signing by Google Play > create upload key

--- keys_files.sh
+++ keys_files.sh
@@ -3,6 +3,8 @@ declare -a FILES=(
 	"google-services.json"
 	"app-signing-release-keystore.keystore"
 	"app-signing-release-keys.properties"
+	"google-play-upload-keystore.keystore"
+	"google-play-upload-keys.properties"
 	"res/values/keys.xml"
 );
 echo "Files:";
