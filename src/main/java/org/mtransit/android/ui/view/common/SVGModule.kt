34dbc48cafa65ae791e7ec1a71be5254b1fb607e
Mathieu Méa <mathieu.mea@gmail.com>
2022-09-21 14:37:08 -0400

    Add SVG support to Glide.

---
+++ src/main/java/org/mtransit/android/ui/view/common/SVGModule.kt
@@ -0,0 +1,27 @@
+package org.mtransit.android.ui.view.common
+
+import android.content.Context
+import android.graphics.drawable.PictureDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.caverock.androidsvg.SVG
+import java.io.InputStream
+
+// https://github.com/bumptech/glide/blob/master/samples/svg/
+@GlideModule
+class SVGModule : AppGlideModule() {
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        registry.apply {
+            register(SVG::class.java, PictureDrawable::class.java, SVGDrawableTranscoder())
+            append(InputStream::class.java, SVG::class.java, SVGDecoder())
+        }
+    }
+
+    // Disable manifest parsing to avoid adding similar modules twice.
+    override fun isManifestParsingEnabled(): Boolean {
+        return false
+    }
+}
