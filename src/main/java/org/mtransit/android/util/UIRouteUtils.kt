0496b65ad8ba25dfdd935cdbad9533a3df12099c
Mathieu Méa <mathieu.mea@gmail.com>
2024-01-20 13:51:34 -0500

    Route short name UI improvements > fix

---
+++ src/main/java/org/mtransit/android/util/UIRouteUtils.kt
@@ -0,0 +1,24 @@
+package org.mtransit.android.util
+
+import android.content.Context
+import org.mtransit.android.commons.SpanUtils
+
+object UIRouteUtils {
+
+    private val FONT_REGULAR = SpanUtils.getNewSansSerifTypefaceSpan()
+    private val FONT_CONDENSED = SpanUtils.getNewSansSerifCondensedTypefaceSpan()
+
+    @JvmStatic
+    fun decorateRouteShortName(
+        @Suppress("UNUSED_PARAMETER") context: Context,
+        rsn: String,
+    ): CharSequence {
+        return SpanUtils.setAll(
+            rsn,
+            getRouteShortNameFont(rsn),
+        )
+    }
+
+    fun getRouteShortNameFont(rsn: String) = if (rsn.length < 4) FONT_REGULAR else FONT_CONDENSED
+
+}
