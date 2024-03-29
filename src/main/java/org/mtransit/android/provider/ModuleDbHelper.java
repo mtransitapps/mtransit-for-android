42f3f69703ccda54e230763141dda5f767373f4f
Mathieu Méa <mathieu.mea@gmail.com>
2024-03-29 16:28:05 -0400

    Migrated some module from Train to Light Rail + Updated colors...

--- src/main/java/org/mtransit/android/provider/ModuleDbHelper.java
+++ src/main/java/org/mtransit/android/provider/ModuleDbHelper.java
@@ -30,7 +30,7 @@ public class ModuleDbHelper extends MTSQLiteOpenHelper {
 	/**
 	 * Override if multiple {@link ModuleDbHelper} in same app.
 	 */
-	public static final int DB_VERSION = 86;
+	public static final int DB_VERSION = 87;
 
 	/**
 	 * Override if multiple {@link ModuleDbHelper} implementations in same app.
