4adc1d53fb66f154bdb7bf42313204dfba52ca58
Mathieu Méa <mathieu.mea@gmail.com>
2024-12-18 09:28:40 -0500

    Application > convert to Kotlin + use Dispatchers.Default to launch Maps init()

---
+++ app-android/src/main/java/org/mtransit/android/ui/MTApplication.kt
@@ -0,0 +1,67 @@
+package org.mtransit.android.ui
+
+import android.app.Application
+import android.content.Context
+import android.os.Build
+import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import org.mtransit.android.BuildConfig
+import org.mtransit.android.analytics.AnalyticsUserProperties
+import org.mtransit.android.analytics.IAnalyticsManager
+import org.mtransit.android.commons.LocaleUtils
+import org.mtransit.android.commons.MTLog
+import org.mtransit.android.dev.CrashReporter
+import org.mtransit.android.dev.IStrictMode
+import org.mtransit.android.dev.LeakDetector
+import org.mtransit.android.ui.view.MapsInitializerUtil
+import org.mtransit.android.util.NightModeUtils
+import org.mtransit.commons.CommonsApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class MTApplication : Application(), MTLog.Loggable {
+
+    companion object {
+        private val LOG_TAG: String = MTApplication::class.java.simpleName
+    }
+
+    override fun getLogTag() = LOG_TAG
+
+    @Inject
+    lateinit var leakDetector: LeakDetector
+
+    @Inject
+    lateinit var crashReporter: CrashReporter
+
+    @Inject
+    lateinit var strictMode: IStrictMode
+
+    @Inject
+    lateinit var analyticsManager: IAnalyticsManager
+
+    private val mainScope = MainScope()
+
+    override fun attachBaseContext(base: Context) {
+        // Demo mode: cannot access manager instance before application context is attached => set in activities
+        val fixedBase = LocaleUtils.attachBaseContextApplication(base)
+        super.attachBaseContext(fixedBase)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        CommonsApp.setup(true)
+        if (BuildConfig.DEBUG) {
+            this.leakDetector.setup(this)
+        }
+        NightModeUtils.setDefaultNightMode(this, null)
+        LocaleUtils.onApplicationCreate(this)
+        this.crashReporter.setup(!BuildConfig.DEBUG)
+        this.strictMode.setup() // uses crash reporter
+        this.analyticsManager.setUserProperty(AnalyticsUserProperties.DEVICE_MANUFACTURER, Build.MANUFACTURER)
+        mainScope.launch(Dispatchers.Default) {
+            MapsInitializerUtil.initMap(this@MTApplication)
+        }
+    }
+}
