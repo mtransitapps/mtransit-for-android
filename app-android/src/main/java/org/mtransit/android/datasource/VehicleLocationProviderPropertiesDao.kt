c802c7d217c90629f4f2ad4a12fd5472c4d44a23
Mathieu Méa <mathieu.mea@gmail.com>
2025-12-08 11:41:54 -0500

    fix

--- app-android/src/main/java/org/mtransit/android/datasource/VehicleLocationProviderPropertiesDao.kt
+++ app-android/src/main/java/org/mtransit/android/datasource/VehicleLocationProviderPropertiesDao.kt
@@ -9,18 +9,18 @@ import org.mtransit.android.data.VehicleLocationProviderProperties
 @Dao
 interface VehicleLocationProviderPropertiesDao : BaseDao<VehicleLocationProviderProperties> {
 
-    @Query("SELECT * FROM service_update_provider_properties")
+    @Query("SELECT * FROM vehicle_location_provider_properties")
     suspend fun getAllVehicleLocationProvider(): List<VehicleLocationProviderProperties>
 
-    @Query("SELECT * FROM service_update_provider_properties")
+    @Query("SELECT * FROM vehicle_location_provider_properties")
     fun readingAllVehicleLocationProvider(): LiveData<List<VehicleLocationProviderProperties>>
 
-    @Query("SELECT * FROM service_update_provider_properties WHERE authority = :authority")
+    @Query("SELECT * FROM vehicle_location_provider_properties WHERE authority = :authority")
     suspend fun getVehicleLocationProvider(authority: String): VehicleLocationProviderProperties
 
-    @Query("SELECT * FROM service_update_provider_properties WHERE target_authority = :targetAuthority")
+    @Query("SELECT * FROM vehicle_location_provider_properties WHERE target_authority = :targetAuthority")
     suspend fun getTargetAuthorityVehicleLocationProvider(targetAuthority: String): List<VehicleLocationProviderProperties>
 
-    @Query("SELECT * FROM service_update_provider_properties WHERE target_authority = :targetAuthority")
+    @Query("SELECT * FROM vehicle_location_provider_properties WHERE target_authority = :targetAuthority")
     fun readingTargetAuthorityVehicleLocationProviders(targetAuthority: String?): LiveData<List<VehicleLocationProviderProperties>>
 }
