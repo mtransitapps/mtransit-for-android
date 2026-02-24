9a6e42079f5e3ddf80acfa53a2cb8484a6e8f8f7
Mathieu Méa <mathieu.mea@gmail.com>
2026-02-24 08:27:02 -0500

    PR comments

--- app-android/src/main/java/org/mtransit/android/ui/splash/SplashScreenViewModel.kt
+++ app-android/src/main/java/org/mtransit/android/ui/splash/SplashScreenViewModel.kt
@@ -58,7 +58,7 @@ class SplashScreenViewModel @Inject constructor(
     private suspend fun getAndUpdateAppOpenCounts(): Int = withContext(Dispatchers.IO) {
         var appOpenCounts = defaultPrefRepository.getValue(PREF_USER_APP_OPEN_COUNTS, PREF_USER_APP_OPEN_COUNTS_DEFAULT)
         appOpenCounts++
-        var appOpenLast = defaultPrefRepository.getValue(PREF_USER_APP_OPEN_LAST, PREF_USER_APP_OPEN_LAST_DEFAULT).toInstant()
+        var appOpenLast = defaultPrefRepository.getValue(PREF_USER_APP_OPEN_LAST, PREF_USER_APP_OPEN_LAST_DEFAULT).millisToInstant()
         val sevenDaysAgo = TimeUtilsK.currentInstant() - 7.days
         val dailyUser = sevenDaysAgo < appOpenLast && appOpenCounts > 10 // opened in the last 7 days
         appOpenLast = TimeUtilsK.currentInstant()
