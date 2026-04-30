86243bd87230c0716efaa625d5c03a2bd4a06832
Mathieu Méa <mathieu.mea@gmail.com>
2026-04-30 08:36:45 -0400

    Revert "Analytics: revert `RDSRouteFragment` -> `RTSRouteFragment` screen vie…"
    This reverts commit b20eba438f7a4d35cc76c39a31fc498f36876169.

--- app-android/src/main/java/org/mtransit/android/ui/rds/route/RDSRouteFragment.kt
+++ app-android/src/main/java/org/mtransit/android/ui/rds/route/RDSRouteFragment.kt
@@ -1,4 +1,4 @@
-@file:JvmName("RDSRouteFragment") // ANALYTICS // do not change to avoid breaking tracking
+@file:JvmName("RTSRouteFragment") // ANALYTICS // do not change to avoid breaking tracking
 package org.mtransit.android.ui.rds.route
 
 import android.app.PendingIntent
