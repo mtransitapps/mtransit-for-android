4d45d1c1c97abc89205ddf0d757525c99cbf308a
Mathieu Méa <mathieu.mea@gmail.com>
2026-05-19 09:08:46 -0400

    Fix crash in purchase dialog when trying to use lifecycle (#271)

--- app-android/src/main/java/org/mtransit/android/ui/fragment/PurchaseDialogFragmentExt.kt
+++ app-android/src/main/java/org/mtransit/android/ui/fragment/PurchaseDialogFragmentExt.kt
@@ -1,12 +1,17 @@
 package org.mtransit.android.ui.fragment
 
+import android.annotation.SuppressLint
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.mtransit.android.ad.IAdManager
 
+@SuppressLint("DeprecatedCall") // FIXME migrate fragment to AndroidX
+@Suppress("DEPRECATION") // FIXME migrate fragment to AndroidX
 fun PurchaseDialogFragment.onResumeKt(iAdManager: IAdManager) {
-    this.lifecycleScope.launch(Dispatchers.IO) {
+    val parentActivity = this.activity as? FragmentActivity ?: return
+    parentActivity.lifecycleScope.launch {
+        if (!isAdded) return@launch
         iAdManager.refreshRewardedAdStatus(this@onResumeKt)
     }
 }
