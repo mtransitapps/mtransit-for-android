9894ef17a960a7624d745d7795d54001993eaceb
Mathieu Méa <mathieu.mea@gmail.com>
2026-05-19 08:47:40 -0400

    PR comments & lint fix

--- app-android/src/main/java/org/mtransit/android/ui/fragment/PurchaseDialogFragmentExt.kt
+++ app-android/src/main/java/org/mtransit/android/ui/fragment/PurchaseDialogFragmentExt.kt
@@ -1,15 +1,17 @@
 package org.mtransit.android.ui.fragment
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.mtransit.android.ad.IAdManager
 
+@SuppressLint("DeprecatedCall") // FIXME migrate fragment to AndroidX
+@Suppress("DEPRECATION") // FIXME migrate fragment to AndroidX
 fun PurchaseDialogFragment.onResumeKt(iAdManager: IAdManager) {
-    @Suppress("DEPRECATION") // FIXME migrate fragment to AndroidX
     val parentActivity = this.activity as? FragmentActivity ?: return
-    parentActivity.lifecycleScope.launch(Dispatchers.IO) {
+    parentActivity.lifecycleScope.launch {
+        if (!isAdded) return@launch
         iAdManager.refreshRewardedAdStatus(this@onResumeKt)
     }
 }
