b708d64ff52e780e774da1c1b73f74f9c34f8d28
Mathieu Méa <mathieu.mea@gmail.com>
2026-06-11 09:49:01 -0400

    Analytics > + `mt_button_click`

---
+++ app-android/src/main/java/org/mtransit/android/analytics/AnalyticsUserProperties.kt
@@ -0,0 +1,15 @@
+package org.mtransit.android.analytics
+
+interface AnalyticsUserProperties {
+    companion object {
+        const val OPEN_APP_COUNTS = "mt_app_open_counts"
+
+        const val DEVICE_MANUFACTURER = "mt_device_manufacturer"
+
+        const val FAVORITES_COUNT = "mt_favorites_count"
+
+        const val FAVORITE_FOLDER_COUNT = "mt_favorite_folder_count"
+
+        const val MODULES_COUNT = "mt_modules_count"
+    }
+}
---
+++ app-android/src/main/java/org/mtransit/android/analytics/IAnalyticsManager.kt
@@ -0,0 +1,18 @@
+package org.mtransit.android.analytics
+
+import androidx.annotation.MainThread
+import androidx.annotation.Size
+
+@Suppress("unused")
+interface IAnalyticsManager {
+    fun setUserProperty(@Size(min = 1L, max = 24L) name: String, value: Int)
+
+    fun setUserProperty(@Size(min = 1L, max = 24L) name: String, value: String)
+
+    fun logEvent(@Size(min = 1L, max = 40L) name: String)
+
+    fun logEvent(@Size(min = 1L, max = 40L) name: String, params: AnalyticsEventsParamsProvider?)
+
+    @MainThread
+    fun trackScreenView(page: AnalyticsScreen)
+}
