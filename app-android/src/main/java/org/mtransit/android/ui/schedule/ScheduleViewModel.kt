0596b50581e5add7c8aefd21867f9f1836e72370
Mathieu Méa <mathieu.mea@gmail.com>
2026-07-11 16:27:46 -0400

    fix

--- app-android/src/main/java/org/mtransit/android/ui/schedule/ScheduleViewModel.kt
+++ app-android/src/main/java/org/mtransit/android/ui/schedule/ScheduleViewModel.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.mtransit.android.commons.ColorUtils
 import org.mtransit.android.commons.MTLog
 import org.mtransit.android.commons.data.RouteDirectionStop
