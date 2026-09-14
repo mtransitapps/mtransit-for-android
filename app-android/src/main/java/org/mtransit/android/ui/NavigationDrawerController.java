23636e0ff6683fe25026eae4f5c60ba54a2a52cb
Mathieu Méa <mathieu.mea@gmail.com>
2026-09-14 15:22:17 -0400

    fix

--- app-android/src/main/java/org/mtransit/android/ui/NavigationDrawerController.java
+++ app-android/src/main/java/org/mtransit/android/ui/NavigationDrawerController.java
@@ -756,7 +756,7 @@ class NavigationDrawerController implements MTLog.Loggable, NavigationView.OnNav
 		private final WeakReference<DrawerLayout> drawerLayoutWR;
 
 		InnerOnBackPressedCallback(@NonNull DrawerLayout drawerLayout) {
-			super(!drawerLayout.isDrawerOpen(GravityCompat.START));
+			super(drawerLayout.isDrawerOpen(GravityCompat.START));
 			this.drawerLayoutWR = new WeakReference<>(drawerLayout);
 			drawerLayout.addDrawerListener(this);
 		}
