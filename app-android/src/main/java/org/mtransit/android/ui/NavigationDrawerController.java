2ae03e62cb55786180c049c343c8972fd5c0de89
Mathieu Méa <mathieu.mea@gmail.com>
2026-09-14 10:29:30 -0400

    fix

--- app-android/src/main/java/org/mtransit/android/ui/NavigationDrawerController.java
+++ app-android/src/main/java/org/mtransit/android/ui/NavigationDrawerController.java
@@ -211,13 +211,6 @@ class NavigationDrawerController implements MTLog.Loggable, NavigationView.OnNav
 			EdgeToEdgeKt.applyStatusBarsHeightEdgeToEdge(drawerHeaderStatusBarBg);
 		}
 		this.drawerLayout = mainActivity.findViewById(R.id.drawer_layout);
-		if (UIFeatureFlags.F_EDGE_TO_EDGE) {
-			this.drawerLayout.setDrawerLockMode(
-					this.drawerLayout.isDrawerOpen(GravityCompat.START) ? DrawerLayout.LOCK_MODE_UNLOCKED
-							: DrawerLayout.LOCK_MODE_LOCKED_CLOSED, // drawer swipe gesture interferes with edge-to-edge gestures
-					GravityCompat.START
-			);
-		}
 		try {
 			this.drawerLayout.setDrawerShadow(ContextCompat.getDrawable(mainActivity, R.drawable.drawer_shadow), GravityCompat.START);
 		} catch (Resources.NotFoundException nfe) { // seen on Android 4, 5 & 7
@@ -235,6 +228,13 @@ class NavigationDrawerController implements MTLog.Loggable, NavigationView.OnNav
 		}
 		this.innerOnBackPressedCallback = new InnerOnBackPressedCallback(this.drawerLayout);
 		mainActivity.getOnBackPressedDispatcher().addCallback(this.innerOnBackPressedCallback);
+		if (UIFeatureFlags.F_EDGE_TO_EDGE) {
+			this.drawerLayout.setDrawerLockMode(
+					this.drawerLayout.isDrawerOpen(GravityCompat.START) ? DrawerLayout.LOCK_MODE_UNLOCKED
+							: DrawerLayout.LOCK_MODE_LOCKED_CLOSED, // drawer swipe gesture interferes with edge-to-edge gestures
+					GravityCompat.START
+			);
+		}
 		finishSetupAsync();
 	}
 
